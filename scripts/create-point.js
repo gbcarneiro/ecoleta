9acd6b2112ca2492866e132ccda807db5057b279
gbcarneiro <52725610+gbcarneiro@users.noreply.github.com>
2020-06-04 17:06:21 -0300

    third day: had been finished the page of collect points sign-up

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -21,9 +21,12 @@ function getCities(event) {
   const ufValue = event.target.value
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+  citySelect.innerHTML = "<option value>Selecione a cidade</option>"
+  citySelect.disabled = true
+
   fetch(url).then(res => res.json()).then(cities => {
     for (const city of cities) {
-      citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+      citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
     }
     citySelect.disabled = false
   })
@@ -33,3 +36,49 @@ function getCities(event) {
 document
   .querySelector('select[name=uf]')
   .addEventListener('change', getCities)
+
+//take the items that the user wants to collect 
+const itemsToCollect = document.querySelectorAll('.items-grid li') 
+
+for (const item of itemsToCollect) {
+  item.addEventListener('click', handleSelectedItem)
+}
+
+const collectedItems = document.querySelector('input[name=items]')
+let selectedItems = []
+
+function handleSelectedItem(event) {
+  const itemLi = event.target
+ 
+  //add the class 'selected'
+  itemLi.classList.toggle('selected')
+
+  //verify if exists sellected items, if it 
+  //take the selected items 
+  const itemId = itemLi.dataset.id
+  
+  const alreadySelected = selectedItems.findIndex(item => {
+    const itemFound = item == itemId
+    return itemFound
+  })
+
+  //if  already selected
+  if(alreadySelected >= 0) {
+    //turn off it
+
+    const filteredItems = selectedItems.filter(item => {
+      const itemIsDifferent = item != itemId
+      return itemIsDifferent 
+    })
+  
+    selectedItems = filteredItems
+  } else {
+    //if not, add it 
+    selectedItems.push(itemId)
+  }
+
+  console.log(selectedItems)
+
+  //refresh the hidden imput 
+  collectedItems.value = selectedItems 
+}
